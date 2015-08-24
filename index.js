73f60c3a714a1e37757e6199b7dc22c8e625137e
Mustafa <mgords@gmail.com>
2015-08-24 18:48:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-var port = process.env.PORT || 5000;
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-server.listen(port);
+var portheroku = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
+var server = app.listen(portheroku);
+var io = require('socket.io').listen(server);
+
 app.get('/', function (req, res) {
 res.sendFile(__dirname + '/index.html');
 });
 console.log('Working!');
 var user_id = 0;
 var user_count = 0;
-var users = [];
 io.sockets.on('connection', function (socket) {
     var user = addUser();
+	socket.emit("welcome",{wname:user.name, wid:user.uid});
     io.sockets.emit("usercount", { user_count: user_count });
     socket.on('disconnect', function () {
-        removeUser(user);
+        removeUser();
     });
     socket.on("send_mouse_pos", function(data){
         socket.broadcast.emit("send_draw",{drawarray:data.mouse_pos,username:user.name,userid:user.uid});
     });
     socket.on("get_name", function(data){
         user.name = data.username;
-        socket.emit("welcome",user);
+        socket.emit("welcome",{wname:user.name, wid:user.uid});
     });
     socket.on("message_sent", function(data){
         io.sockets.emit("message_sent_correctly",{msc:data.sentuserid});
@@ -41,16 +42,9 @@ var addUser = function() {
         uid: user_id,
         name: ""
     }
-    users.push(user);
     return user;
 }
-var removeUser = function(user) {
+var removeUser = function() {
     user_count -= 1;
     io.sockets.emit("usercount", { user_count: user_count });
-    for(var i=0; i<users.length; i++) {
-        if(user.uid === users[i].uid) {
-            users.splice(i, 1);
-            return;
-        }
-    }
 }
